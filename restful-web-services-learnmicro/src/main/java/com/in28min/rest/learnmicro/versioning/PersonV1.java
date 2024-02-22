e3bb84135794f807fd2ad8ebb7ae76a13402fa69
jrushi2010 <jrushi2010@gmail.com>
2024-02-22 17:55:13 +0530

    implemented Versioning of Rest Api

---
+++ restful-web-services-learnmicro/src/main/java/com/in28min/rest/learnmicro/versioning/PersonV1.java
@@ -0,0 +1,26 @@
+package com.in28min.rest.learnmicro.versioning;
+
+public class PersonV1 {
+	
+	private String name;
+
+	public PersonV1(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return "PersonV1 [name=" + name + "]";
+	}
+
+
+	
+	
+	
+
+}
